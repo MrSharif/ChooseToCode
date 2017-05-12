676865187c51575011bd671336d0a906345e962f
Fashi Sharif <fashi.sharif@gmail.com>
2017-05-11 22:57:34 -0500

    Added Java script and some Tags in HTML file

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 function onLoad(){
-    alert("Funtion onLoad Called");
+    //alert("Funtion onLoad Called");
+    document.getElementById("timestamp").innerHTML= Date();
+}
+
+function checkNumber(){
+    var theNumber, theMessage;
+
+    theNumber = document.getElementById("smallnumber").value;
+
+    //if x is  Not a number or less then one or greater than 10.
+    if(theNumber < 1 || theNumber > 10) {
+        theMessage = "Number was expected to be between 1 and 10";
+    } else if(isNaN(theNumber)) {
+        theMessage = "Please enter a Number";
+    } else{
+        theMessage = "Number is Good";
+    }
+    
+    document.getElementById("numberMessage").innerHTML = theMessage;
 }
